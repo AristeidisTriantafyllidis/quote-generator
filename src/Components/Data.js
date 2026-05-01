2780ce9224d2d8c1700b38eade0ddf06f5d855b8
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-05-01 17:58:54 +0300

    Create the data bas

---
+++ src/Components/Data.js
@@ -0,0 +1,34 @@
+const Data = [{
+    quote: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse"
+},
+{
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein"
+},
+{
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill"
+},
+{
+    quote:"You can never cross the ocean until you have the courage to lose sight of the shore.",
+    author:"Christopher Columbus"
+},
+{
+    quote:"You miss 100% of the shots you don’t take.",
+    author:"Wayne Gretzky"
+},
+{
+    quote:"Eighty percent of success is showing up.",
+    author:"Woody Allen"
+},
+{
+    quote:"Whatever the mind of man can conceive and believe, it can achieve.",
+    author:"Napoleon Hill"
+},
+{
+    quote:"Winning isn’t everything, but wanting to win is.",
+    author:"Vince Lombardi"
+}
+]
+export default Data

c82dc17240754b4215211b2c01d165ade1ef7426
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-05-01 21:12:36 +0300

    did the fuctionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Data from './Components/Data';
+import React from 'react';
 function App() {
+ 
+ const[quotes,setQuotes]=React.useState(Data[0])
+ function changeQuote(){
+    const number = Math.floor(Math.random() * Data.length);
+  setQuotes(Data[number])
+
+ }
   return (
+
     <div className="App">
-      <h1> test</h1>
+      <p>{quotes.quote}</p>
+      <p>{quotes.author}</p>
+      
+      <button onClick={changeQuote}>Generate Random Quote</button>
     </div>
   );
 }
